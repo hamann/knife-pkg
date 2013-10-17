41adbdf445a95af0d44dc3acb26d0229f401e7ce
Holger Amann <holger@sauspiel.de>
2013-10-17 16:28:38 +0200

    add dry_run to pkg options

--- lib/chef/knife/pkg_base.rb
+++ lib/chef/knife/pkg_base.rb
@@ -42,6 +42,7 @@ class Chef
       pkg_options = Hash.new
       pkg_options[:sudo] = config[:sudo_required]
       pkg_options[:verbose] = config[:verbose] || config[:dry_run] || config[:pkg_verbose]
+      pkg_options[:dry_run] = config[:dry_run]
       pkg_options
     end
 
